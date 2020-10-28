829903657669f159e3b5fdb70c1fee64b72411a1
i35186513 <bilgeurcuk@std.iyte.edu.tr>
2020-10-28 11:51:27 +0000

    example4

--- Lab3/example4.py
+++ Lab3/example4.py
@@ -2,9 +2,9 @@ ticket_price = 3
 age= int(input("What is your age?"))
 
 if age<6 and age>60:
-  print("FREE!")
-elif 6=<age=< 18:
-  print( ticket_price - ticket_price/1/2)
+  print("It is FREE!")
+elif 6<=age and age<= 18:
+  print("Your ticket price is", ticket_price - ticket_price/1/2,"TL." )
 else:
-  print(ticket_price)
+  print("Your ticket price is", ticket_price,"TL.")
 
