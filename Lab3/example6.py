d255b7f2e27addc67f047931542eb0a0d7e1b1ba
c76464294 <73020486+c76464294@users.noreply.github.com>
2020-10-29 00:09:33 +0300

    Update example6.py

--- Lab3/example6.py
+++ Lab3/example6.py
@@ -4,12 +4,15 @@ c = int(input("c="))
 delta =(b**2 - 4*a*c)
 x1= (-b - delta**1/2)/ (2*a)
 x2= (-b + delta**1/2)/ (2*a)
-x1i= ((-b -(-delta**1/2))/2*a)
-
- if 0< delta:
-  print(x1, x2)
-elif 0==delta:
-    print(x1)
-elif delta <0:
-  
-
+#U will find the exact value of x1 and x2,whatever it is complex or not.
+#So when you enter a,b,c for any equation which doesnt have any reel root,x1 and x2 will give u the complex numbers...
+#whose i's part representing by j in computer. for example 5+3i is same thing as 5.0+3.0j.
+#Therefore, you dont need to write any special 'elif'statement for delta<0.
+#Here is the code, u can easily understand.
+if delta>0:
+ print('There are two reel roots:',x1,x2)
+elif delta==0:
+ print('There is only one reel root:',x1)
+#This 'else'statement will be executed when only delta<0.:
+else:
+ print('No reel roots,two complex roots:',x1,x2) 
