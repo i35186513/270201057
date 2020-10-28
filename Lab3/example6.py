d2bd3a1680117a624e31aef1bc4abd394b543947
i35186513 <bilgeurcuk@std.iyte.edu.tr>
2020-10-28 16:22:24 +0000

    delta<0 kısmını yapamıyorum

---
+++ Lab3/example6.py
@@ -0,0 +1,15 @@
+a = int(input("a="))
+b = int(input("b="))
+c = int(input("c="))
+delta =(b**2 - 4*a*c)
+x1= (-b - delta**1/2)/ (2*a)
+x2= (-b + delta**1/2)/ (2*a)
+x1i= ((-b -(-delta**1/2))/2*a)
+
+ if 0< delta:
+  print(x1, x2)
+elif 0==delta:
+    print(x1)
+elif delta <0:
+  
+
