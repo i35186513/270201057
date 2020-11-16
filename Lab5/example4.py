a9931b836968084fff8533c17633d6d35aeb285c
i35186513 <bilgeurcuk@std.iyte.edu.tr>
2020-11-16 10:02:17 +0000

    example4

---
+++ Lab5/example4.py
@@ -0,0 +1,15 @@
+password= "963tuv"
+
+while True:
+  user_entry = input("Enter password: ")
+  if user_entry == password:
+    print("Welcome")
+    break
+  elif user_entry == "help" :
+    print(password[0])
+  else:
+    print("Wrong")
+
+
+
+
